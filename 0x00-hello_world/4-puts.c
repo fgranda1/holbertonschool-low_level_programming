47fda5142b79cb7a6818d17ab0ae84cf013368b1
Felipe Granda R <felipegranda@hotmail.com>
2020-09-17 00:51:42 +0000

    Hello, puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <string.h>
+
+int main(void)
+{
+
+	char msg[55];
+
+	strcpy(msg, "\"Programming is like building a multilingual puzzle\n");
+
+	puts(msg);	
+	return (0);
+}
