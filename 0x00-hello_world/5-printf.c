b55f0471836c7f1837eba687b6357c00edd88bfb
Felipe Granda R <felipegranda@hotmail.com>
2020-09-17 02:34:48 +0000

    Hello, printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <string.h>
+/**
+ * main - main block
+ * Return: 0
+ */
+int main(void)
+{
+
+	char msg[55];
+
+	strcpy(msg, "with proper grammar, but the outcome is a piece of art,");
+
+	puts(msg);
+	return (0);
+}
